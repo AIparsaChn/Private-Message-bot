9cbb5d8fb18f04b1fe28d1518f29f5081b1a0323
AIparsaChn <chrkhabyparsa@gmail.com>
2025-08-28 20:42:27 +0200

    feat: create initial configuration for the bot

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+
+import telebot
+from telebot.async_telebot import AsyncTeleBot, ExceptionHandler
+
+logger = telebot.async_telebot.logger
+logger.setLevel("INFO")
+
+class BotExceptionHandler(ExceptionHandler):
+    async def handle(self, exception):
+        logger.info(exception, exc_info=True)
+
+TOKEN = os.environ.get("bot_token", None)
+if not TOKEN:
+    raise ValueError("The token doesn't exist.")
+
+bot = AsyncTeleBot(
+    token=TOKEN,
+    exception_handler=BotExceptionHandler()
+)
+
+
+if __name__ == "__main__":
+    asyncio.run(bot.infinity_polling())
+
+
